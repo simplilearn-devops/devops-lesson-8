31196d9d11ea2ea958477e847f36164d8cfc37e9
simplilearn-devops <sandilya@simplilearn.com>
2017-01-08 05:01:20 -0700

    Changed test value

--- DevOps/test/devops/business/DockerDataTests.java
+++ DevOps/test/devops/business/DockerDataTests.java
@@ -5,21 +5,24 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.github.dockerjava.api.model.Event;
+import com.github.dockerjava.api.model.EventType;
 
 public class DockerDataTests {
-	private static final Event CREATE_EVENT = new Event( "create", "8100f5fdc9784e78799ecf5a7e8961349d2ce21e655ad7247c4034764ae7e62e", "centos", 1483609742L ) ;
+	private Event createEvent  ;
 	private DockerData dockerData ;
 	
 	@Before
 	public void setup() {
 		dockerData = new DockerData() ;
+		createEvent = new Event( "create", "8100f5fdc9784e78799ecf5a7e8961349d2ce21e655ad7247c4034764ae7e62e", "centos", 1483609742L ) ;
+		createEvent.withType( EventType.CONTAINER ) ;
 	}
 	
 	@Test
 	public void dockerCreateContainerEvent() {
-		Docker docker = dockerData.processDockerData( CREATE_EVENT ) ;
+		Docker docker = dockerData.processDockerData( createEvent ) ;
 		System.out.println( docker ) ;
-		Assert.assertEquals( "Create", "Container CREATE from centos", docker.getMessage().substring( 0, 28 ) ) ;
+		Assert.assertEquals( "Create", "CONTAINER CREATE from centos", docker.getMessage().substring( 0, 28 ) ) ;
 	}
 
 }
